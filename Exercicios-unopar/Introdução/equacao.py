0104100f1d97f0fdcc95993fe25f32f4e0fedda6
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2021-06-02 20:04:05 -0300

    Introdução à Linguagem

---
+++ Exercicios-unopar/Introdução/equacao.py
@@ -0,0 +1,7 @@
+a = 2
+b = 3
+c = 5
+x = input("Digite um número: ")
+x = float(x)
+y = a * x ** 2 + b * x + c
+print(f"o resultado de Y para {x} = {y}") 
---
+++ Exercicios-unopar/Introdução/opmath.py
@@ -0,0 +1,22 @@
+x = 10
+y = 5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+op1 = 2+3*5
+print(f"o resultado da operação 1 é {op1}")
+op2 = (2+3)*5
+print(f"o resultado da operação 2 é {op2}")
+op3 = 4/2**2
+print(f"o resultado da operação 3 é {op3}")
+op4 = (4/2)**2
+print(f"o resultado da operação 4 é {op4}")
+op5 = 13%3+4
+print(f"o resultado da operação 5 é {op5}")
+print(type(op1))
+print(type(op2))
+print(type(op3))
+print(type(op4))
+print(type(op5))
