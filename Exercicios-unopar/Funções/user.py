a5330895b521f8f1bf0b66e1a6a9a727a922fd09
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2021-06-04 18:13:36 -0300

    Funções

---
+++ Exercicios-unopar/Funções/user.py
@@ -0,0 +1,10 @@
+def mensagem(disciplina, curso):
+    print(f"Minha primeira função em Python desenvolvida na disciplina: {disciplina}, do curso: {curso}.")
+    
+mensagem("Python", "ADS")
+
+def somar(a, b):
+  s = a + b
+  print(f"A soma de {a} + {b} = {s}")
+
+somar(5, 2)
