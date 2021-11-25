ab0eda7f37e5315e880baf2e797cd2d1199b760e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2021-11-25 19:12:36 -0300

    Curso Python - Aula 1

---
+++ CursoP3/aula1.py
@@ -0,0 +1,12 @@
+# Inicio
+print('Hello World!')
+# Depois do olá
+print('Linha 1')
+print('Linha 2')
+# Uma linha
+# Outra linha
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+print('Linha 6')
+# Final
